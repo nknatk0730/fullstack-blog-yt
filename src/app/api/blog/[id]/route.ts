21a088c5019f783bfef94651f38e6b86ec865a08
nknatk0730 <nknatk0730@gmail.com>
2024-03-13 22:18:06 +0900

    fix

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { main } from "../route";
 import { PrismaClient } from "@prisma/client";
+import main from "../route";
 
 const prisma = new PrismaClient();
 
